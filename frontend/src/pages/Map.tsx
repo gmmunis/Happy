d6eae65f8e2b0828bb40e7711a00784053de1068
guilherme munis <gmmunis@hotmail.com>
2021-01-22 14:46:05 -0300

    Trabalhando com rotas entre as páginas

---
+++ frontend/src/pages/Map.tsx
@@ -0,0 +1,12 @@
+
+
+
+function Map() {
+  return (
+    <div>
+      <h1>Hello Happy!</h1>
+    </div>
+  );
+}
+
+export default Map;
---
+++ frontend/src/routes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Map from './pages/Map';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/map" component={Map} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
