bbf29e80cf3e9f8fea42e06fb14f12f710d42b59
guilherme munis <gmmunis@hotmail.com>
2021-01-22 15:42:55 -0300

    Criando a página de mapa

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -1,10 +1,42 @@
 
+import mapMarkerImg from '../images/logo.svg';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
+import 'leaflet/dist/leaflet.css';
+
+import '../styles/pages/map.css';
 
 function Map() {
   return (
-    <div>
-      <h1>Hello Happy!</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <MapContainer
+        center={[-23.6307621,-46.6802013]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?acces_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} /> */}
+      </MapContainer>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color = "#FFF" />
+      </Link>
     </div>
   );
 }
